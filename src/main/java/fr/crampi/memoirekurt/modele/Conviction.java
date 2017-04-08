ad29f1e7225839acbe328abcf0aed76984698916
crampi <crampi@masterPC>
2017-04-08 22:15:52 +0200

    Change-Id: Ie1191f004b7a1b65f339626df374ff469fc2e556

---
+++ src/main/java/fr/crampi/memoirekurt/modele/Conviction.java
@@ -0,0 +1,16 @@
+package fr.crampi.memoirekurt.modele;
+
+/**
+ * Created by crampi on 08/04/17.
+ */
+public class Conviction {
+    private String conviction;
+
+    public String getConviction() {
+        return conviction;
+    }
+
+    public void setConviction(String conviction) {
+        this.conviction = conviction;
+    }
+}
